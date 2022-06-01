fe4d3a923dab78952d78f9d21eec969cfc396cbb
Rkanraeden <lecortezc@gmail.com>
2022-06-01 18:51:58 -0400

    green test ok

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -7,4 +7,12 @@ class OrderTest < ActiveSupport::TestCase
     assert !order.number.nil?
   end
     
+  test "number must be unique" do
+    user = User.create(email: "user@example.com", password: "12345678")
+    order = Order.create(user_id: user.id)
+    duplicated_order = order.dup
+    assert_not duplicated_order.valid?
+  end
+
+
 end

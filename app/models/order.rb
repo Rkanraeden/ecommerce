80c4391898755a54a20800d4ba3556b37cf6ab70
Rkanraeden <lecortezc@gmail.com>
2022-06-16 21:36:04 -0400

    cart shop ok

--- app/models/order.rb
+++ app/models/order.rb
@@ -32,11 +32,18 @@ class Order < ApplicationRecord
   def add_product(product_id, quantity)
     product = Product.find(product_id)
     if product && (product.stock > 0)
-
       order_items.create(product_id: product.id, quantity: quantity, price: product.price)
+      total_value
     end
   end
 
+  def total_value
+    sum = 0
+    order_items.each do |item|
+      sum += item.price
+    end
+    update_attributes(total_value: sum)
+  end
 
 
 end
